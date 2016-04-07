9de1451f4df067fba53a04844c0948c78709cbc9
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-04-07 15:36:51 -0700

    format date and time

--- front-end-code-foo/app/assets/javascripts/welcome.js
+++ front-end-code-foo/app/assets/javascripts/welcome.js
@@ -1,8 +1,18 @@
 $( document ).ready(function() {
+  defaultLoad();
   showArticles();
   showVideos();
   changeBackgroundImage();
+  formatDate();
+  formatTime();
 });
+
+var defaultLoad = function(){
+  $("#videolist").show();
+  $("#articlelist").hide();
+  $("#videobutton").css({"background-color": "#d3222a", "color": "white"})
+  $("#articlebutton").css({"background-color": "white", "color": "#d3222a"})
+};
   var showArticles = function(){
     $("#articlebutton").on("click",function(){
       $("#articlelist").show();
@@ -35,3 +45,33 @@ var changeBackgroundImage = function(){
     $("#"+thisDiv).css({"background-image": 'none', "color": "black"})
   });
 };
+
+var formatDate = function(){
+  var dates = {"1": "Jan", "2": "Feb", "3": "Mar", "4": "Apr", "5": "May", "6": "Jun"}
+  for (i = 0; i < $(".date").length; i++){
+    var oldFormat = $(".date")[i];
+    var month = oldFormat.innerHTML[6]
+    var test_str = oldFormat.innerHTML;
+    var start_pos = test_str.indexOf('2') + 8;
+    var end_pos = test_str.indexOf('T',start_pos);
+    var text_to_get = test_str.substring(start_pos,end_pos)
+    var newDate = dates[month] +" "+text_to_get
+    oldFormat.innerHTML = newDate;
+  }
+}
+
+var formatTime = function(){
+  for (i=0; i< $(".time").length; i++){
+    var oldFormat = $(".time")[i]
+    console.log(oldFormat.innerHTML)
+    if (oldFormat.innerHTML < 60){
+      oldFormat.innerHTML = "0"+":"+ oldFormat.innerHTML
+    }
+    else if (oldFormat.innerHTML%60 < 10) {
+      oldFormat.innerHTML = Math.floor(oldFormat.innerHTML/60) +":"+"0"+""+oldFormat.innerHTML%60
+    }
+    else {
+      oldFormat.innerHTML = Math.floor(oldFormat.innerHTML/60) +":"+oldFormat.innerHTML%60
+    }
+  }
+}
