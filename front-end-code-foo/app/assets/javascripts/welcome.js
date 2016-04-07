08e8d7ae3e84d6012e0803685648e17860211277
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-04-06 17:00:26 -0700

    change background image on hover js

--- front-end-code-foo/app/assets/javascripts/welcome.js
+++ front-end-code-foo/app/assets/javascripts/welcome.js
@@ -1,11 +1,14 @@
 $( document ).ready(function() {
   showArticles();
   showVideos();
+  changeBackgroundImage();
 });
   var showArticles = function(){
     $("#articlebutton").on("click",function(){
       $("#articlelist").show();
       $("#videolist").hide();
+      $("#articlebutton").css({"background-color": "#d3222a", "color": "white"})
+      $("#videobutton").css({"background-color": "white", "color": "#d3222a"})
       console.log("working")
     })
   };
@@ -14,6 +17,22 @@ $( document ).ready(function() {
     $("#videobutton").on("click",function(){
       $("#videolist").show();
       $("#articlelist").hide();
+      $("#videobutton").css({"background-color": "#d3222a", "color": "white"})
+      $("#articlebutton").css({"background-color": "white", "color": "#d3222a"})
       console.log("working")
     })
   };
+
+var changeBackgroundImage = function(){
+  $(".child").on("mouseenter",function(){
+    var thisDiv = $(this)[0].id
+    var img = $(this)[0].children[0].innerHTML
+    $("#"+thisDiv).css({"background-image": 'url(' + img + ')', "color": "white"});
+    console.log(thisDiv)
+  });
+  $(".child").on("mouseleave",function(){
+    var thisDiv = $(this)[0].id
+    var img = $(this)[0].children[0].innerHTML
+    $("#"+thisDiv).css({"background-image": 'none', "color": "black"})
+  });
+};
